c2ee96d2299754f34d1d335037acd7bdf24989a8
Nick Burch <nick.burch@flectech.uk>
2020-03-25 22:40:59 +0000

    Nearly working

--- README.md
+++ README.md
@@ -8,11 +8,18 @@ REST API to create a matching ticket in JIRA. Finally, it sends an email
 back to Tawk.to, to update the ticket there with details of the JIRA
 version.
 
+This should probably work for an on-premise JIRA instance too, but is more
+typically for JIRA Cloud use.
+
 ## Installation
 *TODO*
 
 ## Configuration
 *TODO*
+
+## JIRA Configuration
+You need to get a JIRA API Token, eg from 
+https://id.atlassian.com/manage/api-tokens
 
 ## Based on
 https://www.scalyr.com/blog/azure-functions-in-python-a-simple-introduction/
--- TicketCreated/__init__.py
+++ TicketCreated/__init__.py
@@ -1,10 +1,16 @@
 import os
 import json
 import logging
+import requests
 import azure.functions as func
+from requests.auth import HTTPBasicAuth
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+# What Environment / Application settings we require
+required_keys = ["JiraInstance"] # TODO Rest
+
+# Process the request
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Tawk.To WebHook request received')
 
@@ -16,9 +22,22 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # Get the ticket details
     ticket = data["ticket"]
     logging.info("%s - %s - %s", ticket["id"], ticket["humanId"], ticket["subject"])
+    jref = createTicketInJIRA(ticket)
 
-    sendEmail("Testing", "Sent via Sendgrid")
-    return func.HttpResponse(f"Ticket created in JIRA for %s" % ticket["subject"])
+    if jref:
+        sendUpdateEmail(ticket, jref)
+        return func.HttpResponse(f"Ticket created in JIRA as %s" % jref)
+    else:
+        return func.HttpResponse("Error creating JIRA ticket", status_code=500)
+
+def sendUpdateEmail(ticket, jref):
+    subject = "[#%d] %s" % (ticket["humanId"], ticket["subject"])
+    viewurl = os.environ.get("JiraInstance") + "browse/" + jref
+    message = """
+This ticket has been logged in JIRA as %s.
+
+To view more details or check the latest status, please visit %s""" % (jref, viewurl)
+    sendEmail(subject, message)
 
 def sendEmail(subject, message):
     logging.info("Sending test email - %s", subject)
@@ -35,5 +54,54 @@ def sendEmail(subject, message):
     except Exception as e:
         logging.error(e.message)
 
-def createJIRATicket(todo):
-    pass
+def createTicketInJIRA(ticket):
+    data = {
+    "fields": {
+        "summary": ticket["subject"],
+        "issuetype": {
+            "id": "10004"
+        },
+        "project": {
+            "key": os.environ.get("JiraProject"),
+        },
+        "description": {
+            "type": "doc",
+            "version": 1,
+            "content": [
+                {
+                "type": "paragraph",
+                "content": [
+                    {
+                    "text": ticket["message"],
+                    "type": "text"
+                    }
+                ]
+                },
+                {
+                "type": "paragraph",
+                "content": [
+                    {
+                    "text": "Note - for further details, including any possible screenshots, "+
+                            "please refer to Ticket %d in Tawk.to" % ticket["humanId"],
+                    "type": "text"
+                    }
+                ]
+                }
+            ]
+        }
+    }}
+
+    url = os.environ.get("JiraInstance") + "rest/api/3/issue"
+
+    r = requests.post(url, json=data,
+            auth=HTTPBasicAuth(os.environ.get("JiraUsername"),
+                                os.environ.get("JiraKey")))
+
+    if (r.status_code < 200 or r.status_code > 299):
+        logging.error(r.status_code)
+        logging.error(r.json())
+        return None
+
+    jref = r.json()["key"]
+    logging.info("Ticket created in JIRA for %d as %s", ticket["humanId"], jref)
+    return jref
