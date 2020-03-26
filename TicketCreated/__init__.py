723e0811f5aa36c5be4f87245bc49a71c243a193
Nick Burch <nick.burch@flectech.uk>
2020-03-26 21:25:40 +0000

    When emailing back to Tawk, need to put the ticket ID (without dashes) in the email address

--- TicketCreated/__init__.py
+++ TicketCreated/__init__.py
@@ -8,7 +8,9 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 # What Environment / Application settings we require
-required_keys = ["JiraInstance"] # TODO Rest
+# TODO Add the rest of these
+# TODO Verify we have all of them at startup
+required_keys = ["JiraInstance"]
 
 # Process the request
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -30,21 +32,29 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     else:
         return func.HttpResponse("Error creating JIRA ticket", status_code=500)
 
+def generateTicketEmail(ticket):
+    (before, after) = os.environ.get("TawkTo_TicketsEmail").split("@")
+    plainId = ticket["id"].replace('-','')
+    email = "%s%s@%s" % (before,plainId,after)
+    logging.info("Generated ticket email <%s> for ticket <%d>", email, ticket["humanId"])
+    return email
+
 def sendUpdateEmail(ticket, jref):
+    toaddr = generateTicketEmail(ticket)
     subject = "[#%d] %s" % (ticket["humanId"], ticket["subject"])
     viewurl = os.environ.get("JiraInstance") + "browse/" + jref
     message = """
 This ticket has been logged in JIRA as %s.
 
 To view more details or check the latest status, please visit %s""" % (jref, viewurl)
-    sendEmail(subject, message)
+    sendEmail(toaddr, subject, message)
 
-def sendEmail(subject, message):
+def sendEmail(toaddress, subject, message):
     logging.info("Sending test email - %s", subject)
 
     message = Mail(
         from_email=os.environ.get("EmailFrom"),
-        to_emails=os.environ.get("TawkTo_TicketsEmail"),
+        to_emails=toaddress,
         subject=subject,
         plain_text_content=message)
     try:
@@ -54,6 +64,7 @@ def sendEmail(subject, message):
     except Exception as e:
         logging.error(e.message)
 
+# TODO Template more of this
 def createTicketInJIRA(ticket):
     data = {
     "fields": {
