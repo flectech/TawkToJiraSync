e544f917170931d5a489d57d6be961f909d103c7
Nick Burch <nick.burch@flectech.uk>
2020-03-27 22:05:09 +0000

    Start on reverse function

---
+++ JiraUpdated/__init__.py
@@ -0,0 +1,15 @@
+import json
+import logging
+import azure.functions as func
+from TicketCreated import settings
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('JIRA WebHook request received')
+
+    # Parse the JSON
+    data = json.loads(req.get_body())
+
+    # TODO Make sense of it
+    logging.info("JIRA said: %s", data)
+
+    return func.HttpResponse("Thank you JIRA!")
